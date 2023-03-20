4e4f75233e243b200735bc4572464af40659c28e
SJ139 <90908149+SJ139@users.noreply.github.com>
2023-03-20 18:22:45 -0400

    Picker Setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { DisplayDifficulty } from "./components/DisplayDifficulty/DisplayDifficulty";
+import {MenuList} from "./components/MenuList/MenuList.js";
+import style from "./style.module.css"
 
-function App() {
+export function App () {
+  // const [currentDifficulty, setCurrentDifficulty] = useState("")
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1> Select Your React Difficulty Level"</h1>
+    <div className={style.workspace}>
+    <MenuList/>
+      <DisplayDifficulty difficulty={"Low"}/>
+    </div>
     </div>
   );
 }
-
-export default App;
