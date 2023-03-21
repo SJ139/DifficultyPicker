e3e04ba9c5c97b63e130491ec299cf3a454031e3
SJ139 <90908149+SJ139@users.noreply.github.com>
2023-03-21 19:21:37 -0400

    Picker Finished

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import { DisplayDifficulty } from "./components/DisplayDifficulty/DisplayDifficulty";
 import {MenuList} from "./components/MenuList/MenuList.js";
 import style from "./style.module.css"
+import { useState } from "react";
 
 export function App () {
-  // const [currentDifficulty, setCurrentDifficulty] = useState("")
+  const [currentDifficulty, setCurrentDifficulty] = useState("Insane");
+
+// 4. Which sets the difficulty to the new state.
+
+  const onMenuListItemClick = (difficulty) =>
+  setCurrentDifficulty(difficulty);
+
+
+  // 3. onItemClick below calls OnMenuListItemClick above, and the component will re-render 
+  // becaause of the change in state.
+
   return (
     <div>
       <h1> Select Your React Difficulty Level"</h1>
     <div className={style.workspace}>
-    <MenuList/>
-      <DisplayDifficulty difficulty={"Low"}/>
+    <MenuList onItemClick = {onMenuListItemClick}/>
+      <DisplayDifficulty difficulty={currentDifficulty}/>
     </div>
     </div>
   );
--- src/components/MenuListItem/MenuListItem.js
+++ src/components/MenuListItem/MenuListItem.js
@@ -14,11 +14,24 @@ export const MenuListItem = (props) => {
     if (isHovered) {
       return "#a5e9ff";
     } else {
+        if(props.isSelected){
+      return "26baea"     
+        }
       return "#eff0ef";
     }
   };
+
+//   2. props.onClick passes through to App.js using props.onClick and
+//   sendint its own difficulty.
+
+  const onItemClick = () =>{
+    props.onClick(props.difficulty)
+  }
+
+//   1. onClick triggers the OnItemClick function above.
   return (
     <div
+      onClick={onItemClick}
       className={s.container}
       onMouseEnter={activate}
       onMouseLeave={deactivate}
